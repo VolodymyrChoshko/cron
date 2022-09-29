3e2b585e69d0371ba69118407ba00b7d906dd089
Pyskur <pyskur1@gmail.com>
2022-09-29 13:18:15 +0100

    Relation Controllers Added.

---
+++ app/Http/Controllers/UsersGroupsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UsersGroups;
+use Illuminate\Http\Request;
+
+class UsersGroupsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UsersGroups  $usersGroups
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UsersGroups $usersGroups)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UsersGroups  $usersGroups
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UsersGroups $usersGroups)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UsersGroups  $usersGroups
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UsersGroups $usersGroups)
+    {
+        //
+    }
+}
