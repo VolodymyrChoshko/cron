8ac99e710cb1b5d1abdee3d795ece9b410a381e6
Pyskur <pyskur1@gmail.com>
2022-10-26 07:19:03 +0100

    email_verification -> verify/email

--- routes/api.php
+++ routes/api.php
@@ -101,7 +101,7 @@ Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
     Route::get('login_required', 'login_required')->name('login');
-    Route::post('email_verification', 'email_verification');
+    Route::post('verify/email', 'email_verification');
 });
     
 
